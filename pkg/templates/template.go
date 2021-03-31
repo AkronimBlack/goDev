b582b7bd498f349aa59e2eed0867273441ebb57b
AkronimBlack <bojan.madzarac@asseco-see.com>
2021-03-31 10:10:27 +0200

    fix template

--- pkg/templates/template.go
+++ pkg/templates/template.go
@@ -128,7 +128,7 @@ import (
   "os"
   "time"
 
-  "github.com/asseco-voice/pusher/shared"
+  "github.com/asseco-voice/{{.Name}}/shared"
   "github.com/gin-contrib/cors"
   "github.com/gin-gonic/gin"
   "github.com/joho/godotenv"
