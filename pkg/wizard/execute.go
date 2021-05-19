d4ef0b89a57e52c27a14fc0055b81726af33a83a
Akronim <bojanmadzaracka@gmail.com>
2021-05-19 21:45:17 +0200

    remove appgenerator

--- pkg/wizard/execute.go
+++ pkg/wizard/execute.go
@@ -1,14 +1,22 @@
 package wizard
 
 import (
+	"fmt"
 	"log"
+	"os"
+	"strings"
+	"text/template"
 
 	"github.com/AkronimBlack/stock/common"
-	"github.com/AkronimBlack/stock/pkg/appgenerator"
 	"github.com/AkronimBlack/stock/pkg/templates"
 )
 
 const (
+	TypeFile = "file"
+	TypeDir  = "dir"
+	//NamePlaceholder is a string the builder will look for and replace with name of full-name of the project
+	NamePlaceholder = "{app_name}"
+
 	ginFramework     = "gin"
 	gorillaFramework = "gorilla"
 	noFramework      = "none"
@@ -40,7 +48,7 @@ func DefaultHTTPFramework() string {
 
 func Execute(opts *Options) {
 	executeOptions = opts
-	config := appgenerator.NewConfig(opts.Name, opts.FullName, opts.Maintainer, opts)
+	config := NewConfig(opts.Name, opts.FullName, opts.Maintainer, opts)
 	common.LogJson(config)
 	for _, x := range getObjectMap() {
 		x.Build(config)
@@ -101,46 +109,46 @@ require (
 
 }
 
-func getObjectMap() []*appgenerator.Object {
-	return []*appgenerator.Object{
+func getObjectMap() []*Object {
+	return []*Object{
 		{
-			Name: appgenerator.NamePlaceholder,
-			Type: appgenerator.TypeDir,
-			SubObjects: []*appgenerator.Object{
+			Name: NamePlaceholder,
+			Type: TypeDir,
+			SubObjects: []*Object{
 				{
 					Name: "api",
-					Type: appgenerator.TypeDir,
-					SubObjects: []*appgenerator.Object{
+					Type: TypeDir,
+					SubObjects: []*Object{
 						{
 							Name: "openapi",
-							Type: appgenerator.TypeDir,
+							Type: TypeDir,
 						},
 						{
 							Name: "proto",
-							Type: appgenerator.TypeDir,
+							Type: TypeDir,
 						},
 					},
 				},
 				{
 					Name: "application",
-					Type: appgenerator.TypeDir,
+					Type: TypeDir,
 				},
 				{
 					Name: "cmd",
-					Type: appgenerator.TypeDir,
-					SubObjects: []*appgenerator.Object{
+					Type: TypeDir,
+					SubObjects: []*Object{
 						{
-							Name: appgenerator.NamePlaceholder,
-							Type: appgenerator.TypeDir,
-							SubObjects: []*appgenerator.Object{
+							Name: NamePlaceholder,
+							Type: TypeDir,
+							SubObjects: []*Object{
 								{
 									Name:     "main.go",
-									Type:     appgenerator.TypeFile,
+									Type:     TypeFile,
 									Template: getTemplate("main.go"),
 								},
 								{
 									Name:     "main_test.go",
-									Type:     appgenerator.TypeFile,
+									Type:     TypeFile,
 									Template: getTemplate("main_test.go"),
 								},
 							},
@@ -149,82 +157,181 @@ func getObjectMap() []*appgenerator.Object {
 				},
 				{
 					Name: "docker",
-					Type: appgenerator.TypeDir,
-					SubObjects: []*appgenerator.Object{
+					Type: TypeDir,
+					SubObjects: []*Object{
 						{
 							Name:     "Dockerfile",
-							Type:     appgenerator.TypeFile,
+							Type:     TypeFile,
 							Template: getTemplate("Dockerfile"),
 						},
 					},
 				},
 				{
 					Name: "domain",
-					Type: appgenerator.TypeDir,
+					Type: TypeDir,
 				},
 				{
 					Name: "infrastructure",
-					Type: appgenerator.TypeDir,
-					SubObjects: []*appgenerator.Object{
+					Type: TypeDir,
+					SubObjects: []*Object{
 						{
 							Name: "transport",
-							Type: appgenerator.TypeDir,
-							SubObjects: []*appgenerator.Object{
+							Type: TypeDir,
+							SubObjects: []*Object{
 								{
 									Name: "http",
-									Type: appgenerator.TypeDir,
+									Type: TypeDir,
 								},
 								{
 									Name: "grpc",
-									Type: appgenerator.TypeDir,
+									Type: TypeDir,
 								},
 								{
 									Name: "amqp",
-									Type: appgenerator.TypeDir,
+									Type: TypeDir,
 								},
 							},
 						},
 						{
 							Name: "repositories",
-							Type: appgenerator.TypeDir,
+							Type: TypeDir,
 						},
 					},
 				},
 				{
 					Name: "logs",
-					Type: appgenerator.TypeDir,
+					Type: TypeDir,
 				},
 				{
 					Name:     "docker-compose.yml",
-					Type:     appgenerator.TypeFile,
+					Type:     TypeFile,
 					Template: getTemplate("docker-compose.yml"),
 				},
 				{
 					Name:     "Dockerfile",
-					Type:     appgenerator.TypeFile,
+					Type:     TypeFile,
 					Template: getTemplate("Dockerfile.dev"),
 				},
 				{
 					Name:     "go.mod",
-					Type:     appgenerator.TypeFile,
+					Type:     TypeFile,
 					Template: getTemplate("go.mod"),
 				},
 				{
 					Name:     ".env",
-					Type:     appgenerator.TypeFile,
+					Type:     TypeFile,
 					Template: getTemplate(".env"),
 				},
 				{
 					Name:     ".env.example",
-					Type:     appgenerator.TypeFile,
+					Type:     TypeFile,
 					Template: getTemplate(".env"),
 				},
 				{
 					Name:     ".gitignore",
-					Type:     appgenerator.TypeFile,
+					Type:     TypeFile,
 					Template: getTemplate(".gitignore"),
 				},
 			},
 		},
 	}
+}
+
+//NewConfig config constructor
+func NewConfig(name, fullName, maintainer string, templateData interface{}) *Config {
+	return &Config{
+		Name:         name,
+		FullName:     fullName,
+		Maintainer:   maintainer,
+		TemplateData: templateData,
+	}
+}
+
+//Config config struct for generators
+type Config struct {
+	Name         string      `json:"name"`
+	FullName     string      `json:"full_name"`
+	Maintainer   string      `json:"maintainer"`
+	TemplateData interface{} `json:"template_data"`
+}
+
+type Builder interface {
+	Build() error
+}
+
+type Object struct {
+	Name       string
+	Type       string
+	SubObjects []*Object
+	Template   func() []byte
+}
+
+//Build start building
+func (o *Object) Build(config *Config) error {
+	o.replacePlaceholder(config)
+	log.Printf("Generating %s ", o.Name)
+	var err error
+	switch o.Type {
+	case TypeFile:
+		err = o.generateFile(config)
+	case TypeDir:
+		log.Println("Making directory", o.Name)
+		err = os.MkdirAll(o.Name, os.ModePerm)
+	}
+	if err != nil {
+		return err
+	}
+	if o.SubObjects == nil {
+		return nil
+	}
+	for _, x := range o.SubObjects {
+		x.Name = fmt.Sprintf("%s/%s", o.Name, x.Name)
+		err = x.Build(config)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (o *Object) replacePlaceholder(config *Config) {
+	var name string
+	parts := strings.Split(o.Name, "/")
+	if len(parts) > 1 {
+		name = parts[len(parts)-1]
+	} else {
+		name = o.Name
+	}
+	if name == NamePlaceholder {
+		var replacement string
+		log.Println("name is placeholder. replacing with ", config.FullName)
+		if config.Name != "" {
+			replacement = config.Name
+		} else {
+			replacement = config.FullName
+		}
+
+		if len(parts) > 1 {
+			parts[len(parts)-1] = replacement
+			o.Name = strings.Join(parts, "/")
+			return
+		}
+		o.Name = replacement
+	}
+}
+
+func (o *Object) generateFile(config *Config) error {
+	log.Printf("Creating: %s", o.Name)
+	f, err := os.Create(o.Name)
+	if err != nil {
+		log.Panic(err.Error())
+	}
+	if o.Template != nil {
+		mainTemplate := template.Must(template.New("main").Parse(string(o.Template())))
+		err = mainTemplate.Execute(f, config.TemplateData)
+	}
+	if err != nil {
+		log.Panic(err)
+	}
+	return f.Close()
 }
